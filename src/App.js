b8e96a7dfcef4fab5f775e98184cbcdd130ae7a1
RiceBoy09x <81791469+NonSense96@users.noreply.github.com>
2023-01-13 11:12:51 -0500

    qwdqw
    qwdqw

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navegador from '../src/Components/Navbar'  
+import Catalogo from "../src/Pages/Catalogo";
 function App() {
   return (
     <>
       <Router>
         <Navegador>
-          <Routes>
-              <Route path='/' element={<Home/>}/> 
-              <Route path='/productos'  element={<Products/>}/> 
+          <Routes> 
+              <Route path='/catalogo'  element={<Products/>}/> 
               <Route path='/facturas'  element={<Facturas/>}/>   
               <Route path='/contacto'  element={<Contacto/>}/> 
               <Route path='/informacion'  element={<Informacion/>}/>  
