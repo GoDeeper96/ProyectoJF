a6b386f870167928745b556e85c07ebb8320e6fe
RiceBoy09x <81791469+NonSense96@users.noreply.github.com>
2023-02-21 12:15:41 -0500

    Update 4
    - sliderImagenes

---
+++ src/Components/Slider_Imagenes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Slider_imagenes.module.css'
+
+import { sliderImages } from './SlidersArrayImagenes'
+const Slider_imagenes = (props ) => {
+
+  return (
+    <section className={styles.contenedor__slider}>
+        <div className={styles.slider}>
+            <div className={styles.slider__section}>
+                <img src={sliderImages[props.currentIndex].img} alt={`img${props.currentIndex+1}`} className={styles.slider__img}/>
+            </div>
+        </div>
+        <div className={styles.slider__btn} style={props.btnRightStyle} onClick={props.Siguiente}>{props.rightArrow}</div>
+        <div className={styles.slider__btn} style={props.btnLeftStyle} onClick={props.Anterior}>{props.leftArrow}</div>
+    </section>
+  )
+}
+
+export default Slider_imagenes
