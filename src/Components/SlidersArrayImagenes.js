28df2dc2a64b29fc34897a0a3cbb94251e9989d9
RiceBoy09x <81791469+NonSense96@users.noreply.github.com>
2023-02-21 12:13:41 -0500

    Update 4
    - sliders array imagenees

---
+++ src/Components/SlidersArrayImagenes.js
@@ -0,0 +1,10 @@
+import slider1 from '../img/slider1_index.png'
+import slider2 from '../img/slider2_index.jpg'
+import slider3 from '../img/slider3_index.jpg'
+import slider4 from '../img/slider4_index.jpg'
+export const sliderImages = [
+    {img:slider1},
+    {img:slider2},
+    {img:slider3},
+    {img:slider4},
+  ] 
