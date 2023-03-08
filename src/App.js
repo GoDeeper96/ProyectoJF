58c8c93e2a2555183beb0a34186549cb69a08189
RiceBoy09x <81791469+NonSense96@users.noreply.github.com>
2023-03-08 11:19:42 -0500

    Update 6
    - AGREGADO LINKS DE FOOTER

--- src/App.js
+++ src/App.js
@@ -12,10 +12,15 @@ function App() {
               <Route path='/noticia'  element={<Home/>}/>                 
               <Route path='/nosotros'  element={<Home/>}/>  
               <Route path='/catalogo'  element={<Home/>}/> 
-              <Route path='/recetas'  element={<Home/>}/>  
+              <Route path='/politicas-de-privacidad'  element={<Home/>}/>  
+              <Route path='/cambios-y-devoluciones'  element={<Home/>}/>  
+              <Route path='/terminos-y-condiciones'  element={<Home/>}/>  
+              <Route path='/politicas-de-cookies'  element={<Home/>}/>  
+              <Route path='/libro-de-reclamaciones'  element={<Home/>}/>  
           </Routes> 
       </Router>
   );
 }
 
 export default App;
+
