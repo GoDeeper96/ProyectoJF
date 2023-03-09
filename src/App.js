69394a3ec102d40f5b5c80d4200e721453081d01
RiceBoy09x <81791469+NonSense96@users.noreply.github.com>
2023-03-09 11:54:01 -0500

    Update 7 APP.JS
    - ADDED SOME ROUTES

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navegador from "../src/Components/Navbar";  
 import Catalogo from "../src/Pages/Catalogo";
 import Home from "../src/Pages/Home";
+import Catalogo_seccion from "./Components/Catalogo_seccion";
+import Footer from "./Components/Footer";
+import Contacto from "./Pages/Contacto";
+import Noticias from "./Pages/Noticias"
+import Nosotros from "./Pages/Nosotros"
+import Recetas from "./Pages/Recetas"
 function App() {
   return (
       <Router>
         <Navegador/>
           <Routes> 
               <Route path='/' element={<Home/>}/>
-              <Route path='/contacto'  element={<Home/>}/>
-              <Route path='/noticia'  element={<Home/>}/>                 
-              <Route path='/nosotros'  element={<Home/>}/>  
-              <Route path='/catalogo'  element={<Home/>}/> 
+              <Route path='/contacto'  element={<Contacto/>}/>
+              <Route path='/noticias'  element={<Noticias/>}/>                 
+              <Route path='/nosotros'  element={<Nosotros/>}/>  
+              <Route path='/catalogo'  element={<Catalogo_seccion/>}/> 
+              <Route path='/recetas' element={<Recetas/>}/>
               <Route path='/politicas-de-privacidad'  element={<Home/>}/>  
               <Route path='/cambios-y-devoluciones'  element={<Home/>}/>  
               <Route path='/terminos-y-condiciones'  element={<Home/>}/>  
               <Route path='/politicas-de-cookies'  element={<Home/>}/>  
               <Route path='/libro-de-reclamaciones'  element={<Home/>}/>  
           </Routes> 
+          <Footer/>
       </Router>
   );
 }
