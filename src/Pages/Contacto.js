1d96110733677fc2d05ebd915565d8dab67945f3
RiceBoy09x <81791469+NonSense96@users.noreply.github.com>
2023-03-09 12:18:15 -0500

    UPDATE 7
    UPDATE CATALOGO SECCION

---
+++ src/Pages/Contacto.js
@@ -0,0 +1,126 @@
+import React from 'react'
+import styles from './Contacto.module.css'
+const Contacto = () => {
+  return (
+    <main className={styles.contenedor}>
+    <h1 className={styles.si_centrar}>Contacto</h1>
+
+    <div className={styles.contacto__grid__margin_arriba_bloque_margin_lados_bloques}>
+        <div className={styles.contacto_bg}>
+                <h3 className={styles.contacto__contenido__htres}>Información de contacto</h3>
+            <div className={styles.footer__margin}>
+                <a href="mailto:ventasquimico@inversionesjyfdewjs.com"><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mail footer__redes--posicion conctato--margin_contenido" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <rect x="3" y="5" width="18" height="14" rx="2" />
+                    <polyline points="3 7 12 13 21 7" />
+                </svg></a>
+                <a className={styles.footer__telefonos__a__email} href="mailto:ventasquimico@inversionesjyfdewjs.com">ventasquimico@inversionesjyfdewjs.com</a>
+                </div>
+                <div className={styles.footer__margin}>
+                    <a href="tel:+51946925847"><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-phone footer__redes--posicion conctato--margin_contenido" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                    </svg></a>
+                    <a className={styles.footer__telefonos__a} href="tel:+51946925847">946925847</a>
+                </div>
+
+                <div className={styles.footer__margin}>
+                    <a href="tel:+51994244459"><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-phone footer__redes--posicion conctato--margin_contenido" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                    </svg></a>
+                    <a className={styles.footer__telefonos__a} href="tel:+51994244459">994244459</a>
+                </div>
+
+                <div className={styles.footer__margin}>
+                <a  href="tel:+51960113935"><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-phone footer__redes--posicion conctato--margin_contenido" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                </svg></a>
+                <a className={styles.footer__telefonos__a} href="tel:+51960113935">960113935</a>
+                </div>
+                <div className={styles.footer__margin}>
+                <a href="https://api.whatsapp.com/send?phone=51946925847" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-whatsapp footer__redes--posicion conctato--margin_contenido" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+                    <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
+                </svg></a>
+                <a className={styles.footer__telefonos__a} href="https://api.whatsapp.com/send?phone=51946925847" target="_blank">51946925847</a>
+                </div>
+
+            <div>
+                <p className={styles.footer_p}>Lunes - Sabado de 08:00 am - 18:00 pm.</p>
+                </div>
+
+                <h3 className={styles.contacto__contenido__htres}>Síguenos</h3>
+                <div className={styles.footer__margin}>
+                    <a href="https://www.instagram.com/inversionesjyf_/" target="_blank"> <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-instagram footer__redes--posicion conctato--margin_contenido" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <rect x="4" y="4" width="16" height="16" rx="4" />
+                        <circle cx="12" cy="12" r="3" />
+                        <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+                    </svg></a>
+                    <a className={styles.footer__redes__a} href="https://www.instagram.com/inversionesjyf_/" target="_blank">inversionesjyf_</a>
+                </div>
+    
+                <div className={styles.footer__margin}>
+                    <a href="https://www.facebook.com/inversionesjyfdewjs" target="_blank"> <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-facebook footer__redes--posicion conctato--margin_contenido" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                    </svg></a>
+                    <a className={styles.footer__redes__a} href="https://www.facebook.com/inversionesjyfdewjs" target="_blank">inversionesjyfdewjs</a>
+                </div>
+    </div>
+
+        <form className={styles.formulario}>
+            <div className={styles.campo}>
+                <label className={styles.campo__label} for="email">E-mail</label>
+                <input 
+                    className={styles.campo__field}
+                    type="email" 
+                    placeholder="Por ejemplo: email@dominio.com" 
+                    id="email"
+                />
+            </div>
+
+            <div className={styles.campo}>
+                <label className={styles.campo__label} for="nombre">Nombre</label>
+                <input 
+                    className={styles.campo__field}
+                    type="text" 
+                    placeholder="Por ejemplo: José Sanchez Yagui" 
+                    id="nombre"
+                />
+            </div>
+
+            <div className={styles.campo}>
+                <label className={styles.campo__label} for="telefono">Teléfono</label>
+                <input 
+                    className={styles.campo__field}
+                    type="tel" 
+                    placeholder="Por ejemplo: 965458978" 
+                    id="telefono"
+                />
+            </div>
+
+            <div className={styles.campo}>
+                <label className={styles.campo__label} for="mensaje">Mensaje</label>
+                <textarea 
+                    className={styles.campo__field__textarea}
+                    placeholder="Escribe tu mensaje aquí..." 
+                    id="mensaje"
+                ></textarea>
+            </div>
+
+            <div className={styles.campo__boton}>
+                <input type="submit" value="Enviar" className={styles.boton__primario}/>
+            </div>
+        </form>
+</div>
+    
+</main>
+
+  )
+}
+
+export default Contacto
