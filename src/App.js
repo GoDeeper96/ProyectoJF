f566572f68058622e122c45e825129efc2dbc62e
RiceBoy09x <81791469+NonSense96@users.noreply.github.com>
2023-03-14 12:12:14 -0500

    UPDATE 8
    - AGREGADO CATALOGO CON  SECCIONES Y RUTAS
    - AGREGADO PRODUCTO Y RUTA

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navegador from "../src/Components/Navbar";  
 import Home from "../src/Pages/Home";
+import Catalogo_seccion from "./Components/Catalogo_seccion";
 import Footer from "./Components/Footer";
 import Contacto from "./Pages/Contacto";
-import Noticias from "./Pages/Noticias";
+import Noticias from "./Pages/Noticias"
 import Nosotros from "./Pages/Nosotros"
-import Catalago from "./Pages/Catalogo"
 import Recetas from "./Pages/Recetas"
+import { sliderProductos } from "./Components/ProductosArrayImagenes";
+import ProductoDescripcion from "./Components/ProductoDescripcion";
+import ProductosxSeccion from "./Components/ProductosxSeccion";
 function App() {
   return (
       <Router>
@@ -14,15 +17,18 @@ function App() {
           <Routes> 
               <Route path='/' element={<Home/>}/>
               <Route path='/contacto'  element={<Contacto/>}/>
-              <Route path='/noticia'  element={<Noticias/>}/>                   
+              <Route path='/noticias'  element={<Noticias/>}/>                 
               <Route path='/nosotros'  element={<Nosotros/>}/>  
-              <Route path='/catalogo'  element={<Catalago/>}/> 
+              <Route path='/catalogo'  element={<Catalogo_seccion />}/>
+              <Route path='/catalogo/:seccion'  element={<ProductosxSeccion />}/>
+              <Route path=':producto' element={<ProductoDescripcion/>}/>
               <Route path='/recetas' element={<Recetas/>}/>
               <Route path='/politicas-de-privacidad'  element={<Home/>}/>  
               <Route path='/cambios-y-devoluciones'  element={<Home/>}/>  
               <Route path='/terminos-y-condiciones'  element={<Home/>}/>  
               <Route path='/politicas-de-cookies'  element={<Home/>}/>  
               <Route path='/libro-de-reclamaciones'  element={<Home/>}/>  
+              <Route path='*' element={<h1>Not Found</h1>}/>
           </Routes> 
           <Footer/>
       </Router>
