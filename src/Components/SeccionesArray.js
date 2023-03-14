ee36e2da76bd308111ed7a3f1e12a10c166efb76
RiceBoy09x <81791469+NonSense96@users.noreply.github.com>
2023-03-14 12:34:03 -0500

    UPDATE 8

---
+++ src/Components/SeccionesArray.js
@@ -0,0 +1,21 @@
+import banner1 from '../img/banner1.jpg'
+import banner2 from '../img/banner2.jpg'
+import banner3 from '../img/banner3.jpg'
+import banner4 from '../img/banner4.jpg'
+import banner5 from '../img/banner5.jpg'
+import banner6 from '../img/banner6.jpg'
+import banner7 from '../img/banner7.jpg'
+import banner8 from '../img/banner8.jpg'
+import banner9 from '../img/banner9.jpg'
+export const secciones =
+    [
+        {nombre:"agroindustria",img:banner1},
+        {nombre:"cosmetica",img:banner2},
+        {nombre:"alimenticia",img:banner3},
+        {nombre:"reciclaje",img:banner4},
+        {nombre:"textil",img:banner5},
+        {nombre:"minera",img:banner6},
+        {nombre:"limpieza",img:banner7},
+        {nombre:"reactivos",img:banner8},
+        {nombre:"tratamiento",img:banner9},
+      ]
