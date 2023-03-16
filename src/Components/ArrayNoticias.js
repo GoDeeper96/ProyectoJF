69b08f7b6df9a07ada6754af86351dab455f3c85
RiceBoy09x <81791469+NonSense96@users.noreply.github.com>
2023-03-16 12:13:51 -0500

    UPDATE 9

---
+++ src/Components/ArrayNoticias.js
@@ -0,0 +1,45 @@
+import noticia1 from '../img/noticia1.jpg'
+import noticia2 from '../img/noticia2.jpg'
+import noticia3 from '../img/noticia3.jpg'
+
+export const Noticias = [
+    {
+        imagen:noticia1,
+        nombre:"Intercambio comercial sobrepasa los 169.000 millones de dólares",
+        nombreNoticia:"IntercambioComercial",
+        descripcion:"Según datos del Banco Central difundidos esta mañana, el intercambio comercial alcanzó los US$ 169.176 millones a octubre de 2022, con un aumento del 12% en comparación con los primeros diez meses ...",
+        texto:["Según datos del Banco Central difundidos esta mañana, el intercambio comercial alcanzó los US$ 169.176 millones a octubre de 2022, con un aumento del 12% en comparación con los primeros diez meses de 2021.",
+                    "Las exportaciones nacionales fueron de US$ 80.827 millones, con un aumento del 4,7% (+US$ 3.660 millones), y tuvieron como destino 194 mercados diferentes, con ganancias en los envíos atribuibles a 128 de ellos.",
+                    "Según José Miguel Ahumada, subsecretario de Relaciones Económicas Internacionales, 'la vitalidad de las exportaciones del país sigue sustentada en la oferta no cuprífera, que acumuló ventas al exterior por más de US$ 44.571 millones, lo que indica un crecimiento de 31,7% respecto de 2021.'",
+                    "El carbonato de litio (+US$5.654 millones), el salmón (+US$1.212 millones), los fertilizantes (+US$489 millones), el yodo (+US$396 millones), las cerezas frescas (+US$351 millones), los tableros de fibra de madera (+US$221 millones) y la carne de ave (+US$208 millones) son algunas de las industrias que explican esta expansión. También destacan la madera aserrada, las uvas frescas, el metanol, el nitrato potásico, el equipamiento, la madera contrachapada, los moluscos y crustáceos, los productos metálicos y los neumáticos.",
+                    "El carbonato de litio lidera las exportaciones mineras no relacionadas con el cobre, una pauta que ha persistido durante los últimos ocho meses y que ha aumentado durante el periodo de análisis en un 783,6% (+US$ 5.479 millones). Mientras tanto, las exportaciones de sal y concentrado de molibdeno también aumentaron, en 21,4 y 31,2 millones de dólares, respectivamente.",
+                    "Según el subsecretario Ahumada, 'el litio se ha consolidado rápidamente como un pilar de las exportaciones no tradicionales, presentándose como el gran contrapeso a las variaciones adversas que enfrentan las ventas de cobre al exterior en 2022.'",
+                    "Asimismo, las exportaciones industriales aumentaron en 23,9% (+US$ 5.654 millones), siendo los aumentos de productos químicos, alimenticios y forestales los que impulsaron el crecimiento. Además, las exportaciones de servicios llegaron a 136 mercados de destino por un total de US$ 1,224 millones, con un aumento de 9% (+US$ 97 millones) y lideradas por Estados Unidos (US$ 441 millones), Perú (US$ 214 millones) y Colombia (US$ 94 millones).",
+                    "7.155 empresas declararon ventas en el exterior en los primeros diez meses de 2022, lo que supone un aumento del 5,3% respecto al periodo correspondiente de 2021. Las empresas relacionadas con la industria manufacturera fueron las responsables de este crecimiento, ya que registraron 4.332 exportadores en lo que va del año, 366 más que en 2021. Las mipymes constituyen el 50% de las empresas exportadoras en todo el periodo, mientras que las grandes empresas suponen el 38%.",
+                    "Dado su papel crítico en la valorización de nuestra oferta exportable y en su integración a las cadenas productivas regionales e internacionales, es significativo que el sector manufacturero lidere la expansión del número de empresas exportadoras, según el Subsecretario Ahumada.",
+                    "Nueve de las 16 zonas del país tuvieron aumentos en las exportaciones a nivel regional, destacando Antofagasta (+US$ 5.509 millones), Valparaso (+US$ 2.542 millones), Biobo (+US$ 1.489 millones) y Los Lagos (+US$ 1.201 millones).",
+                    "En comparación con el mismo período de 2021 (+US$ 14.280 millones), las importaciones aumentaron un 19,3%, alcanzando los US$ 88.350 millones. Los rubros de diésel (+US$ 3.409 millones), productos químicos (+US$ 1.189 millones), prendas de vestir (+US$ 1.045), carbón (+US$ 1.012 millones), gasolina (+US$ 909 millones), automóviles (+US$ 728 millones) y gas natural gaseoso (+US$ 612 millones) estuvieron entre las principales importaciones, junto con el cartón y el papel procesado."]
+    },
+
+    {
+        imagen:noticia2,
+        nombre:"Carolyn Bertozzi es la octava mujer que recibe el Novel de Química",
+        nombreNoticia:"NovelQuimica",
+        descripcion:"Carolyn Bertozzi, investigadora y profesora de la Universidad de Stanford, se ha convertido en la octava mujer galardonada con el Premio Nobel de Química en 2022 por su trabajo en la llamada quími ...",
+        texto:["Carolyn Bertozzi, investigadora y profesora de la Universidad de Stanford, se ha convertido en la octava mujer galardonada con el Premio Nobel de Química en 2022 por su trabajo en la llamada 'química de clic', en la que descubrió una forma de combinar y construir moléculas que pueden ayudar a desarrollar mejores tratamientos médicos para enfermedades como el cáncer de mama.",
+                "Junto con Morten Melda y Barry Sharpless, dos investigadores que contribuyeron al desarrollo de la 'Química del clic', Bertozzi recibió el honor.",
+                "'Los medicamentos contra el cáncer que se están examinando ahora en ensayos clínicos tienen una diana mejorada gracias a la investigación. La química ha entrado en la era del funcionalismo gracias a la química de clic y las reacciones bioortogonales. El mayor beneficio para la humanidad está siendo proporcionado por esto 'dijo la declaración del Comité Nobel.",
+                "Carolyn Bertozzi declaró que 'cuando el mundo está en peligro, la química acude al rescate' tras conocer la concesión del Nobel. Según El Pas, afirmó que 'la química es un área de investigación realmente interesante para aquellos que quieren tener un impacto en la biología, en la salud, en los materiales, en el clima, en la sostenibilidad', y recordó a familiares y amigos que tenían enfermedades sin tratar. 'Siempre he creído que, como científica, podría tener un impacto positivo en la salud humana, ya sea ahora, más adelante o incluso después de mi muerte'."]
+    },
+
+    {
+        imagen:noticia3,
+        nombre:"Energía nuclear, el punto final tecnológico a la radiactividad en Chernóbil es posible",
+        nombreNoticia:"EnergiaNuclear",
+        descripcion:"El método sin productos químicos de la startup Exlterra ha reducido la contaminación en un 46% en sólo un año. Con sus incursiones en el Bosque Rojo, uno de los lugares más peligrosos del mundo, y ...",
+        texto:["El método sin productos químicos de la startup Exlterra ha reducido la contaminación en un 46% en sólo un año.",
+                "Con sus incursiones en el Bosque Rojo, uno de los lugares más peligrosos del mundo, y sus robos de material radiactivo como baratijas, los soldados rusos han desatado nuevas preocupaciones.",
+                "La cantidad de radiación en el suelo y el aire se ha reducido drásticamente por primera vez en 35 años gracias a este proyecto.",
+                "Esto es importante, debido a que se sabe que en la madrugada del 26 de abril de 1986, dos explosiones destruyeron la tapa y el techo de la unidad 4 de la central nuclear de Chernóbil (Ucrania), liberando material radiactivo a la atmósfera. La semana siguiente, la fuga, provocada por un feroz incendio en el núcleo del reactor, se extendió en todas direcciones. Al final, hubo que evacuar un área de 3110 kilómetros cuadrados debido a los altos niveles de contaminación por cesio-137."]
+    },
+]
---
+++ src/Components/ArrayRecetas.js
@@ -0,0 +1,26 @@
+import receta1 from '../img/receta1.jpg'
+import receta2 from '../img/receta2.jpg'
+import receta3 from '../img/receta3.jpg'
+
+export const Recetas = [
+    {
+        imagen:receta1,
+        nombre:"Fideos",
+        descripcion:"dasdad",
+        ingredientes:["harina","AcidoAscorbico"]
+    },
+
+    {
+        imagen:receta2,
+        nombre:"Pan",
+        descripcion:"dasddasdad",
+        ingredientes:["harinax","CloritoDeSodio"]
+    },
+
+    {
+        imagen:receta3,
+        nombre:"Hueso",
+        descripcion:"dasasdaddasdad",
+        ingredientes:["hari","BenzoatoDeSodio"]
+    },
+]
