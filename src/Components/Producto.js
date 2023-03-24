200c56bcfdedc07245bdee9b87a1eaa6b6dab838
Sebastian Julon Chamana <81791469+GoDeeper96@users.noreply.github.com>
2023-03-24 10:03:25 -0500

    Update 10
    - modificaciones a producto

--- src/Components/Producto.js
+++ src/Components/Producto.js
@@ -2,17 +2,22 @@ import React from 'react'
 import styles from './Producto.module.css'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+const ProductContainer =styled.div`
+  text-align: center;
+  visibility:${props=>props.visibility};
+  display:${props=>props.display};
+  `
 const Producto = (props) => {
   // const ProductCSS= styled.div`
   // text-align: center;
   // transition: all 0.5s ease-out;
 
   // `;
+  
   return (
-    <main>
-       <div className={styles.si_centrar} > 
+    <ProductContainer key={props.goKey} visibility={props.visibility} display={props.display}> 
                 <Link to={`/${props.nombreProducto}`}>
-                  <img className={styles.recetas__imagen} src={props.url}/>
+                  <img  className={styles.recetas__imagen} src={props.url}/>
                 </Link>
                 {/* <a href="sulfato_cobre.html">
                     
@@ -22,10 +27,7 @@ const Producto = (props) => {
                     <p className={styles.productos__contenido}>{props.precio}</p>
                 </div>
 
-        </div>
-    </main>
+  </ProductContainer>
     
   )
 }
-
-export default Producto
